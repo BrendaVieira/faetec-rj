e415c90dbddfe3e0589fb554c7025c66b9d5cc5b
Brenda Vieira Cardoso <85653420+BrendaVieira@users.noreply.github.com>
2022-06-18 22:46:23 -0300

    Update salario.java

--- salario.java
+++ salario.java
@@ -23,70 +23,71 @@ package javinha;
 
 import java.io.*;
 
-public class salario{
-    public static void main(String[] args) {
+public class salario {
 
-        //variaveis
-        String s = "";
+	public static void main(String[] args) {
+		
+		//variáveis
+		String s = "";
         //Scanner read = new Scanner(System.in);
         int quantidadeHoras, dependentes;
         float salarioHora, salarioBruto, inss, ir, salarioLiquido;
         DataInputStream dado;
 		
-		try{
-            
-			System.out.println("Entre com o valor da hora trabalhada: ");
-            //sal = read.nextFloat();------- scanner read
+		try{System.out.println("Entre com o valor da hora trabalhada: ");
+        //sal = read.nextFloat();------- scanner read
 
-			dado = new DataInputStream(System.in);
-			s = dado.readLine();
-			salarioHora=Float.parseFloat(s); //converte a entrada de texto em numero
-			
-			System.out.println("Entre com a quantidade de horas trabalhadas: ");
-			dado = new DataInputStream(System.in);
-			s = dado.readLine();
-			quantidadeHoras=Integer.parseInt(s);
-			
-			System.out.println("Entre com o numero de dependentes: ");
-			dado = new DataInputStream(System.in);
-			s = dado.readLine();
-			dependentes=Integer.parseInt(s);
-			
-			//calculo e resultado
-            salarioBruto = (quantidadeHoras * salarioHora) + (50 * dependentes);
-			
-            //desconto inss
-			if(salarioBruto<=1000){
-                inss = (float) (salarioBruto * 0.085);     // 8.5/100
-			}
-            else{
-                    inss = (float) (salarioBruto * 0.09);     // 9/100
-				}
-            
-            // desconto ir
-            if (salarioBruto <= 500 ){
-                ir = 0;
-            }
-            else{
-                if((salarioBruto > 500 ) && (salarioBruto <= 1000)){
-                    ir = (float) (salarioBruto * 0.05); // 5/100
-                }
-                else {
-                    ir = (float) (salarioBruto * 0.01);  //
-                }
-            }
-
-            salarioLiquido = salarioBruto - inss - ir;
-            System.out.println("O Salario Liquido e: " + salarioLiquido);
-            //JOptionPane.showMessageDialog(null,"O Salario Liquido e: " + salarioLiquido);
+		dado = new DataInputStream(System.in);
+		s = dado.readLine();
+		salarioHora=Float.parseFloat(s); //converte a entrada de texto em numero
+		
+		System.out.println("Entre com a quantidade de horas trabalhadas: ");
+		dado = new DataInputStream(System.in);
+		s = dado.readLine();
+		quantidadeHoras=Integer.parseInt(s);
+		
+		System.out.println("Entre com o numero de dependentes: ");
+		dado = new DataInputStream(System.in);
+		s = dado.readLine();
+		dependentes=Integer.parseInt(s);
+		
+		//calculo e resultado
+        salarioBruto = (quantidadeHoras * salarioHora) + (50 * dependentes);
+        System.out.println("Salario Bruto: " + salarioBruto);
+        
+        //desconto inss
+		if(salarioBruto<=1000){
+            inss = (float) (salarioBruto * 0.085);     // 8.5/100
 		}
-    }
-}
-		/*captura erros
+        else{
+                inss = (float) (salarioBruto * 0.09);     // 9/100
+			}
+		System.out.println("Desconto INSS: " + inss);
+		
+        // desconto ir
+        if (salarioBruto <= 500 ){
+            ir = 0;
+        }
+        else{
+            if((salarioBruto > 500 ) && (salarioBruto <= 1000)){
+                ir = (float) (salarioBruto * 0.05); // 5/100
+            }
+            else {
+                ir = (float) (salarioBruto * 0.01);  //
+            }
+            System.out.println("Desconto IR: " + ir);
+        }
+
+        salarioLiquido = salarioBruto - inss - ir;
+        System.out.println("Salario Liquido: " + salarioLiquido);
+        //JOptionPane.showMessageDialog(null,"O Salario Liquido e: " + salarioLiquido);
+	}
+		//captura erros
 		catch(IOException e){
 			System.out.println("Erro na entrada de dados" );
 		}
 		catch(NumberFormatException e){
-			System.out.println("Erro na conversao. Digite numeros" );
+			System.out.println("Erro na conversão. Digite números" );
+		}	
+	}
 }
-		*/
