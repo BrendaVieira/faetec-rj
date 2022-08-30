c6d9f82d9e5daa31e4dd69d36ac32241406e33ca
Brenda Vieira Cardoso <85653420+BrendaVieira@users.noreply.github.com>
2022-08-29 21:47:34 -0300

    Add files via upload

---
+++ d4/Repeticao.java
@@ -0,0 +1,11 @@
+public class Repeticao {
+	public static void main(String args[]) {
+		int i = 1;
+		int produto =1;
+		while (i <= 10) {
+		produto=produto*i;
+			i+=2;
+		}
+		System.out.println(produto);
+	}
+}
